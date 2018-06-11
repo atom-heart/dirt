3f1d09d702725fed4386a5334c815ea4f12807cc
Filip Krawczyk <fkrawczyk77@gmail.com>
2018-06-11 15:46:11 +0200

    Add database models

--- db_create.py
+++ db_create.py
@@ -1,27 +1,132 @@
+from datetime import timedelta as td
+
 from project import db
-from project.models import Player, Event
+from project.models import Game, Country, Track, Player, CarClass, Car,\
+                           Weather, Split, Time, Event, EventPlayer, Stage, StageRanking
+
 
 print('Creating new tables...', end=' ')
 
 # Drop all existing database tables
+db.reflect()
 db.drop_all()
 
 # Create new tables
 db.create_all()
 
 # Create players
-players = []
-players.append(Player(playerName='atom'))
-players.append(Player(playerName='player1'))
+players = [
+    Player(name='Filip'),
+    Player(name='Michał')
+]
 
-# Create events
-events = []
-events.append(Event(eventName='Kentish Town Cup'))
-events.append(Event(eventName='Birmigham Cup'))
+# Create games
+games = [
+    Game(name='DiRT Rally'),
+    Game(name='DiRT 4')
+]
+
+# Create weather
+weather = [
+    Weather(conditions='Midday/Sunny'),
+    Weather(conditions='Midday/Clear'),
+    Weather(conditions='Afternoon/Cloudy'),
+    Weather(conditions='Morning/Rain'),
+    Weather(conditions='Morning/Overcast')
+]
+
+# Create countries
+countries = [
+    Country(name='Germany', games=games, weather=[weather[0], weather[1]]),
+    Country(name='Greece', games=[games[0]], weather=[weather[1], weather[3]]),
+    Country(name='New Zeland', games=[games[1]], weather=[weather[2], weather[4]])
+]
+
+# Create tracks
+tracks = [
+    Track(name='Flugzeugring', game=games[0], country=countries[0]),
+    Track(name='Koryni Dafni', game=games[0], country=countries[1]),
+    Track(name='Pepe', game=games[1], country=countries[2]),
+    Track(name='Oberstein', game=games[0], country=countries[0])
+]
+
+# Create car car_classes
+car_classes = [
+    CarClass(name='2000s', game=games[0]),
+    CarClass(name='2010s', game=games[0])
+]
+
+# Create cars
+cars = [
+    Car(name='Subaru Impreza 2001', car_classes=[car_classes[0]]),
+    Car(name='Ford Fiesta RS Rally', car_classes=[car_classes[1]])
+]
+
+# Create splits
+splits = [
+    Split(track=tracks[0], weather=weather[0], order=1),
+    Split(track=tracks[0], weather=weather[1], order=2),
+    Split(track=tracks[1], weather=weather[3], order=1),
+    Split(track=tracks[2], weather=weather[2])
+]
+
+# Create times
+times = [
+    Time(time=td(minutes=3, seconds=56, microseconds=333000), player=players[0],
+         car=cars[1], split=splits[0]),
+    Time(time=td(minutes=4, seconds=12, microseconds=107000), player=players[1],
+         car=cars[0], split=splits[0]),
+    Time(time=td(minutes=3, seconds=45, microseconds=333000), player=players[0],
+         car=cars[1], split=splits[1]),
+    Time(time=td(minutes=3, seconds=51, microseconds=107000), player=players[1],
+         car=cars[0], split=splits[1]),
+    Time(time=td(minutes=6, seconds=45, microseconds=730000), player=players[0],
+         car=cars[1], split=splits[2]),
+    Time(time=td(minutes=6, seconds=33, microseconds=900000), player=players[1],
+         car=cars[0], split=splits[2]),
+]
+
+# Create event
+event = Event(name='Kentish Town Cup 2018', game=games[0], car_classes=car_classes)
+
+event_players = [
+    EventPlayer(order=2, car=cars[1]),
+    EventPlayer(order=1, car=cars[0])
+]
+
+event_players[0].player = players[0]
+event_players[1].player = players[1]
+
+event.event_players.extend(event_players)
+
+# Create stages
+stages = [
+    Stage(order=1, event=event, country=countries[0], splits=splits[:2]),
+    Stage(order=2, event=event, country=countries[1], splits=splits[2:3])
+]
+
+# Create stage ranking
+stage_rankings = [StageRanking(time_total=times[0].time + times[2].time + times[4].time), StageRanking(time_total=times[1].time + times[3].time)]
+
+stage_rankings[0].player = players[0]
+stage_rankings[1].player = players[1]
+
+stages[0].stage_rankings.extend(stage_rankings)
 
 # Add to database
 db.session.add_all(players)
-db.session.add_all(events)
+db.session.add_all(games)
+db.session.add_all(weather)
+db.session.add_all(countries)
+db.session.add_all(tracks)
+db.session.add_all(car_classes)
+db.session.add_all(cars)
+db.session.add_all(splits)
+db.session.add_all(times)
+db.session.add_all(event_players)
+db.session.add(event)
+db.session.add_all(stages)
+db.session.add_all(stage_rankings)
 db.session.commit()
 
 print("Done!")
--- project/models.py
+++ project/models.py
@@ -1,15 +1,213 @@
 from project import db
+from sqlalchemy.ext.hybrid import hybrid_property
 
+
+#### Games ############################################################
+#######################################################################
+
+# Many-to-many relationships ##########################################
+
+games_countries = db.Table('games_countries',
+    db.Column('game_id', db.Integer, db.ForeignKey('games.id')),
+    db.Column('country_id', db.Integer, db.ForeignKey('countries.id'))
+)
+
+cars_car_classes = db.Table('cars_car_classes',
+    db.Column('car_id', db.Integer, db.ForeignKey('cars.id')),
+    db.Column('car_class_id', db.Integer, db.ForeignKey('car_classes.id')),
+)
+
+countries_weather = db.Table('countries_weather',
+    db.Column('country_id', db.Integer, db.ForeignKey('countries.id')),
+    db.Column('weather_id', db.Integer, db.ForeignKey('weather.id'))
+)
+
+
+# Tables ##############################################################
+
+class Game(db.Model):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Relationships
+    countries = db.relationship('Country', secondary=games_countries, backref='games', lazy='dynamic')
+    tracks = db.relationship('Track', backref='game', lazy='dynamic')
+    car_classes = db.relationship('CarClass', backref='game', lazy='dynamic')
+
+
+class Country(db.Model):
+    __tablename__ = 'countries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Relationships
+    tracks = db.relationship('Track', backref='country', lazy='dynamic')
+    weather = db.relationship('Weather', secondary=countries_weather, backref='countries', lazy='dynamic')
+    # Backrefs: games
+
+
+class Track(db.Model):
+    __tablename__ = 'tracks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Foreign keys
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+
+    # Relationships
+    splits = db.relationship('Split', backref='track', lazy='dynamic')
+    # Backrefs: game, country
+
+
+class Weather(db.Model):
+    __tablename__ = 'weather'
+
+    id = db.Column(db.Integer, primary_key=True)
+    conditions = db.Column(db.String, nullable=False)
+
+    # Relationships
+    splits = db.relationship('Split', backref='weather', lazy='dynamic')
+    # Backrefs: tracks
+
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Relationships
+    times = db.relationship('Time', backref='car', lazy='dynamic')
+    # Backrefs: car_classes
+
+
+class CarClass(db.Model):
+    __tablename__ = 'car_classes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+    # Foreign keys
+    cars = db.relationship('Car', secondary=cars_car_classes, backref='car_classes', lazy='dynamic')
+    # Backrefs: game
+
+
+class Split(db.Model):
+    __tablename__ = 'splits'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer, nullable=True)
+
+    # Foreign keys
+    track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'))
+    weather_id = db.Column(db.Integer, db.ForeignKey('weather.id'))
+    stage_id = db.Column(db.Integer, db.ForeignKey('stages.id'))
+
+    # Relationships
+    times = db.relationship('Time', backref='split', lazy='dynamic')
+    # Backrefs: track, weather, stage
+
+
+class Time(db.Model):
+    __tablename__ = 'times'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Interval, nullable=False)
+
+    # Foreign keys
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    car_id = db.Column(db.Integer, db.ForeignKey('cars.id'))
+    split_id = db.Column(db.Integer, db.ForeignKey('splits.id'))
+    # Backrefs: player, car, split
+
+
+#### Events ###########################################################
+#######################################################################
+
+# Many-to-many relationships ##########################################
+
+class EventPlayer(db.Model):
+    __tablename__ = 'events_players'
+
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'), primary_key=True)
+    order = db.Column(db.Integer, nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('cars.id'))
+
+    # Relationships
+    car = db.relationship('Car')
+
+
+class StageRanking(db.Model):
+    __tablename__ = 'stages_rankings'
+
+    stage_id = db.Column(db.Integer, db.ForeignKey('stages.id'), primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'), primary_key=True)
+    time_total = db.Column(db.Interval, nullable=False)
+    points = db.Column(db.Interval, nullable=True)
+
+    # Relationships
+    player = db.relationship('Player')
+
+
+events_car_classes = db.Table('events_car_classes',
+    db.Column('event_id', db.Integer, db.ForeignKey('events.id')),
+    db.Column('car_class_id', db.Integer, db.ForeignKey('car_classes.id'))
+)
+
+
+# Tables ##############################################################
 
 class Player(db.Model):
     __tablename__ = 'players'
 
-    playerId = db.Column(db.Integer, primary_key=True)
-    playerName = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Relationships
+    times = db.relationship('Time', backref='player', lazy='dynamic')
+    events = db.relationship('EventPlayer', backref='player', lazy='dynamic')
 
 
 class Event(db.Model):
     __tablename__ = 'events'
 
-    eventId = db.Column(db.Integer, primary_key=True)
-    eventName = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+    # Relationships
+    game = db.relationship('Game')
+    car_classes = db.relationship('CarClass', secondary=events_car_classes)
+    event_players = db.relationship('EventPlayer', backref='event', lazy='dynamic')
+    stages = db.relationship('Stage', backref='event', lazy='dynamic')
+
+    @hybrid_property
+    def players(self):
+        """Returns a dictionary of players in format: {order: player}"""
+        players = {}
+        for event_player in self.event_players.order_by(EventPlayer.order).all():
+            players[event_player.order] = event_player.player
+        return players
+
+
+
+class Stage(db.Model):
+    __tablename__ = 'stages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer, nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+
+    # Relationships
+    country = db.relationship('Country')
+    splits = db.relationship('Split', backref='stage', lazy='dynamic')
+    stage_rankings = db.relationship('StageRanking', backref='stage', lazy='dynamic')
+    # Backrefs: event
