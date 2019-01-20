030340f5e297b90d1ba87ce67ab14a6bf91f5802
Filip Krawczyk <fkrawczyk77@gmail.com>
2019-01-20 22:07:45 +0000

    final fix pls

--- instance/config.py
+++ instance/config.py
@@ -3,10 +3,10 @@ from os import path
 BASEDIR = path.abspath(path.dirname(__file__))
 
 # The app
-DEBUG = True
+DEBUG = False
 SECRET_KEY = '309b73ff14af3782d874a26cf788c3cf92a0b2a28ab3aee0'
 
 # SQLAlchemy
-SQLALCHEMY_DATABASE_URI = 'postgresql://atom:acdc@localhost/dirt'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
