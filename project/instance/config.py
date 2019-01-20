15b583c3b1cd0894f40cb8a39c1f41ceeda7eb53
Filip Krawczyk <fkrawczyk77@gmail.com>
2019-01-20 00:03:46 +0000

    db fix

--- project/instance/config.py
+++ project/instance/config.py
@@ -7,6 +7,6 @@ DEBUG = True
 SECRET_KEY = '309b73ff14af3782d874a26cf788c3cf92a0b2a28ab3aee0'
 
 # SQLAlchemy
-SQLALCHEMY_DATABASE_URI = 'postgresql://atom:acdc@localhost/dirt'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://atom:acdc@localhost/dirt'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = False
