9d51bdd185466c7cc6c2d818abbeef8fa4b302aa
Filip Krawczyk <fkrawczyk77@gmail.com>
2019-01-20 00:32:14 +0000

    y

--- project/instance/config.py
+++ project/instance/config.py
@@ -7,6 +7,6 @@ BASEDIR = path.abspath(path.dirname(__file__))
 SECRET_KEY = '\xc5\xcbmu\xf5\xcd\xbe\xee\xc9a\xac\xe6\x80\x16,\x96\xf4\xcaW\xe9\xa79\xbe\x98'
 
 # SQLAlchem
-SQLALCHEMY_DATABASE_URI = 'postgres://rxqnbsqvyaabnm:09d556f69c5e4f5370d556fd73ada6aa22e56338fdf6e484ca647b370e5737fe@ec2-54-163-246-159.compute-1.amazonaws.com:5432/d816tjvkivk9p7'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
