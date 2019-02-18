6d17832de3604fe45a36ed281c897757f76e9f6c
Filip Krawczyk <fkrawczyk77@gmail.com>
2019-02-18 20:20:17 +0000

    Add code-splitting

--- project/static/js/index.jsx
+++ project/static/js/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -7,7 +7,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../css/layout.css'
 
 import ErrorBoundary from './components/ErrorBoundary'
-import Event from './components/Event'
+import PageLoader from './components/PageLoader'
+const Event = lazy(() => import('./components/Event'))
 import ModalRoot from './components/ModalRoot'
 import store from './store.js'
 
@@ -18,8 +19,10 @@ ReactDOM.render((
     <Provider store={store}>
       <Router>
         <Container>
-          <Route path="/" exact render={() => {return <div>Hello!</div>}} />
-          <Route path="/event/:id" component={Event} />
+          <Suspense fallback={<PageLoader />}>
+            <Route path="/" exact render={() => { return <div>Hello!</div> }} />
+            <Route path="/event/:id" component={Event} />
+          </Suspense>
 
           <Route path="/" component={ModalRoot} />
         </Container>
